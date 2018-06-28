e8c8bf097ac92e590db9acd0eab4ee81528168d7
Garlandal <garlandals@gmail.com>
2018-06-28 19:02:45 +0800

    refactor

--- bilibili.py
+++ bilibili.py
@@ -1,153 +1,159 @@
-#!/usr/bin/env python
-# coding=utf-8
+# coding: utf-8
 
 from __future__ import division
-from collections import defaultdict
 from collections import Counter
 
+import argparse
+import platform
+import json
 import sys
 import re
-import json
-import getopt
 import requests
 import config
-import time
 import copy
 import os
 
 
+def prettify_print(type_, text, status_text=''):
+    color_dict = {
+        'ok': '\033[1;32m',
+        'info': '\033[1;36m',
+        'error': '\033[0;31m',
+        'end': '\033[0m'
+    }
+    s = '%s%s{}%s' % (color_dict[type_], text, color_dict['end'])
+    inner_s = ' {}【{}】'.format(color_dict['ok'], status_text) if status_text else ''
+    print(s.format(inner_s))
+
 
 class Danmu():
+    """Download Danmaku and save as ssa file"""
 
-	"""Download Danmu and save as ssa file"""
+    def __init__(self):
+        self.comment_url = 'http://comment.bilibili.com/'
+        self.script_info = config.SCRIPT_INFO
+        self.v4_styles = config.V4_STYLES
+        self.events = config.EVENTS
+        self.item = ('[Script Info]', '[v4 Styles] ', '[Events]')
+        self.session = requests.Session()
+        self.danmu_dict = {}
+        self.style = {}
+        self.danmu_list = []
 
-	def __init__(self):
-		self.comment_url = 'http://comment.bilibili.com/'
-		self.script_info = config.SCRIPT_INFO
-		self.v4_styles = config.V4_STYLES
-		self.events = config.EVENTS
-		self.item = ('[Script Info]','[v4 Styles] ','[Events]')
+    def _xml_url(self, cid):
+        return ''.join([self.comment_url, str(cid), '.xml'])
 
-		
-	def time_format(self, time, types):
-		time = float(time)
-		if types == '1':
-			endtime = time + 11
-		else:
-			endtime = time + 4
-		start = str(int(time/3600))+':'+str(int(time/60))+':'+str(int(time%60))+'.'+str(time%60-int(time%60))[2:4]
-		end = str(int(endtime/3600))+':'+str(int(endtime/60))+':'+str(int(endtime%60))+'.'+str(endtime%60-int(endtime%60))[2:4]
-		return (start, end)
-		
+    def _parse_xml(self, xml):
+        pat = re.compile(r'<d p="(.*?)">(.*?)</d>')
+        for full_content, text in re.findall(pat, xml):
+            ctime, type_, _, color, _, _, _, pool = full_content.split(',')
+            self.danmu_dict[pool] = [ctime, type_, color, text]
 
-	def get_danmu(self, url):
-		print '\033[1;36m喵娘正在启动 \033[1;32m【 OK 】\033[0m'
-		self.danmu_dict = {}
-		content = requests.get(url).content
-		print '\033[1;36m解析視頻地址 \033[1;32m【 OK 】\033[0m'
-		if len(content) < 10000:
-			return 0
-		pat0 = re.compile(r'bili-cid=(.*)&bili-aid')
-		bili_cid = re.findall(pat0, content)
-		if bili_cid:
-			pass
-		else:
-			pat1 = re.compile(r'cid=(.*)&aid')
-			bili_cid = re.findall(pat1, content)
-		comment_xml = requests.get(''.join([self.comment_url, bili_cid[0], '.xml'])).content
-		comment_pat = re.compile(r'<d p="(.*)">(.*)</d>')
-		for m, n in re.findall(comment_pat, comment_xml):
-			danmu_detail = m.split(',')
-			danmu_content = danmu_detail[0]+','+danmu_detail[1]+','+danmu_detail[3]+','+n 
-			self.danmu_dict[danmu_detail[7]] = danmu_content.split(',')
-		len_danmu = [len(p[3]) for q,p in self.danmu_dict.iteritems()]
-		counts = Counter(len_danmu)
-		self.danmu_counts = sorted(counts.most_common(20))
-		print '\033[1;36m全舰弹幕装填 \033[1;32m......\033[0m'
+    def time_format(self, time, types):
+        time = float(time)
+        if types == '1':
+            endtime = time + 11
+        else:
+            endtime = time + 4
+        start = str(int(time / 3600)) + ':' + str(int(time / 60)) + ':' + str(int(time % 60)) + '.' + str(
+            time % 60 - int(time % 60))[2:4]
+        end = str(int(endtime / 3600)) + ':' + str(int(endtime / 60)) + ':' + str(int(endtime % 60)) + '.' + str(
+            endtime % 60 - int(endtime % 60))[2:4]
+        return (start, end)
 
+    def get_danmu(self, url):
+        prettify_print('info', '喵娘正在启动', 'OK')
+        content = self.session.get(url).content
+        prettify_print('info', '解析視頻地址', 'OK')
+        if len(content) < 10000:
+            prettify_print('error', "Error 解析失败")
+            sys.exit(2)
 
+        pat = re.compile(r'"epInfo":(.*?),"epList')
+        ep_info = re.findall(pat, content)[0]
+        xml_url = self._xml_url(json.loads(ep_info)['cid'])
+        comment_xml = self.session.get(xml_url).content
+        self._parse_xml(comment_xml)
 
-	def deal_danmu(self):
-		self.style = {}
-		white_style = self.v4_styles.get('Style')[0]
-		color_style = self.v4_styles.get('Style')[1]
-		center_style = self.v4_styles.get('Style')[2]
-		centerw_style = self.v4_styles.get('Style')[3]
-		rolling_events = self.events.get('rolling')
-		static_events = self.events.get('static')
-		deal_dict = copy.deepcopy(self.danmu_dict)
-		count_list = [i[0] for i in self.danmu_counts]
-		del_list = [single_id for single_id, content in deal_dict.iteritems() if len(content[3]) not in count_list]
-		map(lambda x: deal_dict.pop(x), del_list)
-		self.danmu_list = []
-		for m,n in deal_dict.iteritems():
-			time_tup = self.time_format(n[0], n[1])
-			if n[2] != '16777215' and n[1] == '1':
-				self.style[n[2]] = color_style.format(style = n[2], color_code = n[2])
-			elif n[2] == '16777215' and n[1] == '1':
-				self.style['16777215'] = white_style.format(style = '16777215')
-			elif n[2] == '16777215' and n[1] != '1':
-				self.style['center16777215'] = centerw_style.format(style = 'center16777215')
-			elif n[2] != '16777215' and n[1] != '1':
-				self.style['center'+n[2]] = center_style.format(style = 'center'+n[2], color_code = n[2])
-			if n[1] == '1' and n[2] == '16777215':
-				self.danmu_list.append(rolling_events.format(start_time = time_tup[0],end_time = time_tup[1],style = '16777215',text = n[3]))
-			elif n[1] == '1' and n[2] != '16777215':
-				self.danmu_list.append(rolling_events.format(start_time = time_tup[0],end_time = time_tup[1],style = n[2], text = n[3]))
-			elif n[1] != '1' and n[2] == '16777215':
-				self.danmu_list.append(static_events.format(start_time = time_tup[0], end_time = time_tup[1],style = 'center16777215', text = n[3]))
-			elif n[1] != '1' and n[2] != '16777215':
-				self.danmu_list.append(static_events.format(start_time = time_tup[0], end_time = time_tup[1],style = 'center'+n[2], text = n[3]))
-		print '\033[1;36m弹幕装填完毕 \033[1;32m【 OK 】\033[0m'
-		
+        danmaku_counter = Counter([len(p[3]) for q, p in self.danmu_dict.iteritems()])
+        self.danmu_counts = sorted(danmaku_counter.most_common(200))
+        prettify_print('info', '全舰弹幕装填', '......')
 
+    def deal_danmu(self):
+        white_style, color_style, center_style, centerw_style = self.v4_styles.get('Style')
+        rolling_events = self.events.get('rolling')
+        static_events = self.events.get('static')
+        deal_dict = copy.deepcopy(self.danmu_dict)
+        count_list = [i[0] for i in self.danmu_counts]
+        del_list = [single_id for single_id, content in deal_dict.iteritems() if len(content[3]) not in count_list]
+        map(lambda x: deal_dict.pop(x), del_list)
+        for m, n in deal_dict.iteritems():
+            time_tup = self.time_format(n[0], n[1])
+            if n[2] != '16777215' and n[1] == '1':
+                self.style[n[2]] = color_style.format(style=n[2], color_code=n[2])
+            elif n[2] == '16777215' and n[1] == '1':
+                self.style['16777215'] = white_style.format(style='16777215')
+            elif n[2] == '16777215' and n[1] != '1':
+                self.style['center16777215'] = centerw_style.format(style='center16777215')
+            elif n[2] != '16777215' and n[1] != '1':
+                self.style['center' + n[2]] = center_style.format(style='center' + n[2], color_code=n[2])
+            if n[1] == '1' and n[2] == '16777215':
+                self.danmu_list.append(
+                    rolling_events.format(start_time=time_tup[0], end_time=time_tup[1], style='16777215', text=n[3]))
+            elif n[1] == '1' and n[2] != '16777215':
+                self.danmu_list.append(
+                    rolling_events.format(start_time=time_tup[0], end_time=time_tup[1], style=n[2], text=n[3]))
+            elif n[1] != '1' and n[2] == '16777215':
+                self.danmu_list.append(
+                    static_events.format(start_time=time_tup[0], end_time=time_tup[1], style='center16777215',
+                                         text=n[3]))
+            elif n[1] != '1' and n[2] != '16777215':
+                self.danmu_list.append(
+                    static_events.format(start_time=time_tup[0], end_time=time_tup[1], style='center' + n[2],
+                                         text=n[3]))
+        prettify_print('info', '弹幕装填完毕', 'OK')
 
-	def write_file(self, filename):
-		with open(filename+'.ssa','w') as f:
-			f.writelines(self.item[0]+'\n')
-			for m, n in self.script_info.iteritems():
-				info = m+': '+n+'\n'
-				f.writelines(info)
-			f.writelines(self.item[1]+'\n')
-			f.writelines('Format'+self.v4_styles.get('Format')+'\n')
-			for p,q in self.style.iteritems():
-				content  = 'Style: '+q+'\n'
-				f.writelines(content)
-			f.writelines(self.item[2]+'\n')
-			for i in self.danmu_list:
-				f.writelines(i+'\n')
-		print '\033[1;36m弹幕装填类型 \033[1;32m【普通】\033[0m'
-
-
-def deal_filename(filename):
-	return re.escape(filename)
+    def write_file(self, filename):
+        with open(filename + '.ssa', 'w') as f:
+            f.writelines(self.item[0] + '\n')
+            for m, n in self.script_info.iteritems():
+                info = m + ': ' + n + '\n'
+                f.writelines(info)
+            f.writelines(self.item[1] + '\n')
+            f.writelines('Format' + self.v4_styles.get('Format') + '\n')
+            for p, q in self.style.iteritems():
+                content = 'Style: ' + q + '\n'
+                f.writelines(content)
+            f.writelines(self.item[2] + '\n')
+            for i in self.danmu_list:
+                f.writelines(i + '\n')
+        prettify_print('info', '弹幕装填类型', '普通')
 
 
+def main(args):
+    filename = args.file
+    url = args.link
+    if not filename or not url:
+        prettify_print('error', "Error 请尝试执行'--help'来获取更多信息")
+        sys.exit(2)
+
+    fan = Danmu()
+    fan.get_danmu(url)
+    fan.deal_danmu()
+    fan.write_file(filename)
+
+    if config.ENALLE_OPEN == 'on':
+        open_command = {
+            'Linux': 'xdg-open',
+            'Darwin': 'open'
+        }[platform.system()]
+        command = ' '.join([open_command, re.escape(filename)])
+        os.system(command)
 
 
-def main():
-	try:
-		opts, args = getopt.getopt(sys.argv[1:], "f:l:hu", ["file=", "link=", "help", "update"])
-	except getopt.GetoptError, e:
-		print " \033[0;31mError 请尝试执行'--help'来获取更多信息 \033[0m"
-		sys.exit(2)
-	fan = Danmu()
-	for opt, arg in opts:
-		if opt in ("-f","--file"):
-			files = arg
-			filename = files.split('.')[0]
-		elif opt in ('-l',"--link"):
-			fan.get_danmu(arg)
-			fan.deal_danmu()
-			fan.write_file(filename)
-		elif opt in ("-h", "--help"):
-			print 'this is helpdoc'
-		elif opt in ("-u", "--update"):
-			print 'the data is updating'
-	if config.ENALLE_OPEN == 'on':
-		command = 'xdg-open ' + deal_filename(files)
-		os.system(command)
-
-			
 if __name__ == '__main__':
-	main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', help='local video file')
+    parser.add_argument('link', help='bilibili video page link')
+    args = parser.parse_args()
+    main(args)
--- config.py
+++ config.py
@@ -1,29 +1,29 @@
-#!/usr/bin/env python
-# coding=utf-8
+# coding: utf-8
 
-#设置弹幕加载完成后是否自动打开
+
+# 设置弹幕加载完成后是否自动打开
 ENALLE_OPEN = 'on'
 
 SCRIPT_INFO = {
-	'ScriptType': 'v4.00',
-	'Collisions': 'Normal',
-	'PlayResX': '384',
-	'PlayResY': '288',
-	'Timer': '100.0000',
+    'ScriptType': 'v4.00',
+    'Collisions': 'Normal',
+    'PlayResX': '384',
+    'PlayResY': '288',
+    'Timer': '100.0000',
 }
 
 V4_STYLES = {
-    'Format': 'Name,Fontname,Fontsize,PrimaryColour,SecondaryColour,TertiaryColour,BackColour,Bold,Italic,BorderStyle,Outline,Shadow,Alignment,MarginL,MarginR,MarginV,AlphaLevel,Encoding',
+    'Format': '''Name,Fontname,Fontsize,PrimaryColour,SecondaryColour,TertiaryColour,BackColour,Bold,Italic,BorderStyle,
+                Outline,Shadow,Alignment,MarginL,MarginR,MarginV,AlphaLevel,Encoding''',
     'Style': (
-    	'{style},WenQuanYi Micro Hei,10,16777215,4227327,8404992,00000000,0,0,1,1,0,0,0,0,107,0,136',
-    	'{style},WenQuanYi Micro Hei,10,{color_code},4227327,8404992,16777215,0,0,1,1,0,0,0,0,107,0,136',
-    	'{style},WenQuanYi Micro Hei,10,{color_code},4227327,8404992,16777215,0,0,1,1,0,6,0,0,0,0,136', 
-    	'{style},WenQuanYi Micro Hei,10,16777215,4227327,8404992,00000000,0,0,1,1,0,6,0,0,0,0,136',
-    	)
+        '{style},WenQuanYi Micro Hei,10,16777215,4227327,8404992,00000000,0,0,1,1,0,0,0,0,107,0,136',
+        '{style},WenQuanYi Micro Hei,10,{color_code},4227327,8404992,16777215,0,0,1,1,0,0,0,0,107,0,136',
+        '{style},WenQuanYi Micro Hei,10,{color_code},4227327,8404992,16777215,0,0,1,1,0,6,0,0,0,0,136',
+        '{style},WenQuanYi Micro Hei,10,16777215,4227327,8404992,00000000,0,0,1,1,0,6,0,0,0,0,136',
+    )
 }
 
 EVENTS = {
-	'rolling': 'Dialogue: Marked=0,{start_time},{end_time},{style},NTP,0000,0000,0000,Banner;25;0;50,{text}',
-	'static': 'Dialogue: Marked=0,{start_time},{end_time},{style},NTP,0000,0000,0000,,{text}',
+    'rolling': 'Dialogue: Marked=0,{start_time},{end_time},{style},NTP,0000,0000,0000,Banner;25;0;50,{text}',
+    'static': 'Dialogue: Marked=0,{start_time},{end_time},{style},NTP,0000,0000,0000,,{text}',
 }
-
